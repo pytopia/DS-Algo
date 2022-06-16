64d6b5b73694c222e78a621e428cf85da9407034
shirin <yamani.shirin@ucalgary.ca>
2022-06-15 18:23:58 -0600

    q1 added

--- trie/answers.py
+++ trie/answers.py
@@ -51,3 +51,51 @@ class Trie:
             current = current.children[ch]
         return True
             
+
+# Question 2
+class Trie(object):
+    def __init__(self):
+        
+        self.children = {}
+        self.end = False
+
+class WordDictionary:
+    def __init__(self):
+        self.root = Trie()
+    
+    def addWord(self, word):
+        """
+        :type word: str
+        :rtype: None
+        """
+        current = self.root
+        for ch in word:
+            if ch not in current.children.keys():
+                current.children[ch] = Trie()
+            current = current.children[ch]
+        current.end = True
+
+    def search(self, word):
+        """
+        :type word: str
+        :rtype: bool
+        """
+        def dfs(start_index, root):
+            current = root
+            for i in range(start_index, len(word)):
+                ch = word[i]
+                
+                if ch == ".":
+                    for child in current.children.values():
+                        
+                        if dfs(i+1, child):
+                            return True
+                    return False
+                else:
+                    if ch not in current.children.keys():
+                        return False
+                    current = current.children[ch]
+            return current.end
+        return dfs(0,self.root) #BC we always start by the first letter and the root node
+        
+        
